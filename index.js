f60ef924a6fc5128fee752d59ee317565736df14
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 23:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /* Your code goes here */
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(person){
+  bestCustomer = person
+}
+
+
+function changeleastFavoriteCustomer(){
+  leastFavoriteCustomer = 'blake';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "candy";
+    let favoriteCustomer = "chocolate";
+}
+
+
+
